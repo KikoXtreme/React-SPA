dc3b8f58961e6cec9f2e641a9f25598bfc86ac9b
Kiril Valkov <kiril_dgr@abv.bg>
2023-01-09 12:59:52 +0200

    sendUserData update

--- src/services/Submit.jsx
+++ src/services/Submit.jsx
@@ -1,27 +1,11 @@
 const url = 'https://webhook.site/ee43433e-f646-4057-8cde-c75a788321dc';
 
-// export const submit = async (userData) => {
-//     const response = await fetch(url, {
-//         method: 'POST',
-//         headers: {
-//             'content-type': 'application/json',
-//         },
-//         body: JSON.stringify(userData)
-//     });
-
-//     const result = await response.json();
-
-//     return result;
-// }
-
-export const sendUserData = async (data) => {
+export const sendUserData = async (userData) => {
     try {
-        let currentRequest = fetch(url, {
+        const response = await fetch(url, {
             method: 'POST',
-            body: JSON.stringify(data),
+            body: JSON.stringify(userData)
         });
-
-        const response = await currentRequest;
         const result = await response.json();
 
         return result;
